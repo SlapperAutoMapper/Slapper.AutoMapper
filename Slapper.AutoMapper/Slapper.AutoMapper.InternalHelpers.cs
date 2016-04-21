1690c71fe553e394ee075d0b82a2441ed64056ca
Tony Parcelli <tony@parcelli.com>
2016-04-21 11:15:20 -0500

    Fixed issue with nested object being populated even if all values are null

--- Slapper.AutoMapper/Slapper.AutoMapper.InternalHelpers.cs
+++ Slapper.AutoMapper/Slapper.AutoMapper.InternalHelpers.cs
@@ -489,7 +489,14 @@ namespace Slapper
                             }
                             else
                             {
-                                nestedInstance = Map(newDictionary, nestedInstance, instance);
+                                if (newDictionary.Values.All(v => v == null))
+                                {
+                                    nestedInstance = null;
+                                }
+                                else
+                                {
+                                    nestedInstance = Map(newDictionary, nestedInstance, instance);
+                                }
                             }
 
                             SetMemberValue(member, instance, nestedInstance);
